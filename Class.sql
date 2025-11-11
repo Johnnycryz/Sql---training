b578f4f642616ff99a118f8686a79293b059d120
DataCryz <110091881+Johnnycryz@users.noreply.github.com>
2025-11-11 09:52:12 +0100

    Update Class.sql

--- Class.sql
+++ Class.sql
@@ -31,7 +31,7 @@ select * from employee_salary where salary <> 70000;
 
 # using logical operators in WHERE clause conditons 
 #AND, NOT, OR, BETWEEN, NOT BETWEEN 
-#select * from employee_salary where condition 1 AND/OR/NOT condition 2
+#select * from employee_salary where condition 1  AND/OR/NOT condition 2
 select * from employee_salary;
 select salary from employee_salary;
 SELECT * from employee_salary where employee_id >4 and salary >= 55000;
@@ -39,6 +39,7 @@ SELECT * from employee_salary where employee_id >4 and salary >= 55000 and occup
 
 select * from employee_salary;
 select * from employee_salary where employee_id < 5 or dept_id  = 1;
+select * from employee_salary where employee_id > 5 and dept_id  = 1;
 
 select * from employee_salary;
 select * from employee_salary where salary >= 50000;
@@ -68,4 +69,36 @@ select * from employee_demographics;
 select * from employee_demographics where first_name between 'April' and 'Jerry' ;
 
 select * from employee_demographics where first_name between 'April' and 'Jerry' order by first_name desc;
+
+#10-Nov-2025
+# Using IN operator: 
+select * from employee_demographics;
+select * from employee_demographics where employee_id between 1 and 3;
+select * from employee_demographics where employee_id in (1, 3, 9);
+# this helps to show specific data in a table by specifying attributes in a particular column
+select * from employee_demographics where first_name in ('Ann', 'Mark');
+# this is like a filter to get specific data
+
+#LIKE operators and wildcard characters (%, _)
+#both LIKE and wildcards characters must be used together. the purpose is for pattern matching
+# pecntile (%) is used for multi char or zero character
+# underscore (_) is used for single char
+select * from employee_demographics;
+select * from employee_demographics where first_name like 'A%';
+select * from employee_demographics where first_name like '%A';
+select * from employee_demographics where first_name like 'C%G';
+select * from employee_demographics where first_name like '%n%';
+select * from employee_demographics where first_name like '%nn%';
+select * from employee_demographics where first_name like '%a%';
+# in line 92, 'April' , 'Andy' and 'Ann' are showing in the result because, % can represent either
+# ... zero or multiple character 
+
+select * from employee_demographics where first_name like 'A_';
+# this result shows null because underscore is used for only one character
+select * from employee_demographics where first_name like 'A__';
+select * from employee_demographics where first_name like 'A_r__';
+#underscore is like fill in the gap in english
+#both can be wildcards can be used together
+select * from employee_demographics;
+select * from employee_demographics where first_name like 'A_d%';
 
